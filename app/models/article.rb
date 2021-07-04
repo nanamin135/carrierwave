3ce3e21abb014344633bfb12bf44ad375d9a701d
nanamin135 <nanamin9386@gmail.com>
2021-07-04 23:59:30 +0900

    image

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     belongs_to :user
+    mount_uploader :image, ImageUploader
     #prisenceはオブジェクトが存在すればオブジェクトを返し、オブジェクトが存在しなければnilを返す 
     #validatesはオブジェクトがDBに保存される前にそのデータが正しいかどうかを検証する仕組み 
     #rubyでのlengthは文字列の長さを表す 
